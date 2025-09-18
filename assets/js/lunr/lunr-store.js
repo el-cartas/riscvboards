6f882d508632dca9ed4ca58a5bd02233f86ecfab
alonso <el.cartas@disroot.org>
2025-09-18 19:05:11 -0300

    lol

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "OpenBSD on the mango pi mq pro",
+        "excerpt":"First tutorial (and post) of the site!!! hell yeah. For some reason there is no tutorial on how to install OpenBSD on the mango pi mq pro so i thought this might be of help for some people. Compiling OpenSBI and Uboot I took everything from linux sunxi, triyng to...","categories": ["posts"],
+        "tags": ["sbc","tutorials"],
+        "url": "/posts/2025/09/17/mango-pi-mq-pro-openbsd.html",
+        "teaser": null
+      }]
